35f6a7e926446272f8c284736724e440bd025948
EveryoneHatesAiden <spinelli.aiden@gmail.com>
2021-08-08 18:19:51 -0400

    First post

---
+++ main.js
@@ -0,0 +1,13 @@
+const Discord = require('discord.js');
+
+const client = new Discord.Client();
+
+
+
+client.once('ready', () => {
+    console.log('MiddlemanBot is online!');
+});
+
+
+
+client.login('ODQ3OTk2ODczMDgzMzIyMzc4.YLGMoQ.a4ooGJPaM6kzBOP9QUrA5Rl2qmw')
